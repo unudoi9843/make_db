209925ed2af2a9b75f5bea0c3becaf073680faf8
unudoi9843 <adm97200@gmail.com>
2016-11-17 15:56:07 +0200

    modified 1_query.sql

--- 1_query.sql
+++ 1_query.sql
@@ -8,8 +8,17 @@ select
    persoana.idnp as 'idnp',
    persoana.nume as 'nume',
    persoana.pren as 'prenume',
-   persoana.adr as 'adresa'
+   persoana.adr as 'adresa',
+   post_de_munca.func as 'functia',
+   post_de_munca.fir_angaj as 'firma angajatoare'
      from angajare
+
      join persoana on
        angajare.idnp = persoana.idnp
-       where persoana.idnp = '2008002000987';
+
+     join post_de_munca on
+       post_de_munca.id_post = angajare.id_post
+
+       where persoana.idnp = '2008002000987'
+
+       ;
