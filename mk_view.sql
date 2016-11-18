783e0939dd5895a36d2be4ce8412f3bf5141a768
unudoi9843 <adm97200@gmail.com>
2016-11-18 13:26:55 +0200

    changed mk_view.sql

--- mk_view.sql
+++ mk_view.sql
@@ -3,14 +3,23 @@
 use sql_training;
 
 create view view_1_query as
-  select
+
+select
    angajare.data_ang as 'data angajarii',
    angajare.data_con as 'data concedierii',
    persoana.idnp as 'idnp',
    persoana.nume as 'nume',
    persoana.pren as 'prenume',
-   persoana.adr as 'adresa'
+   post_de_munca.func as 'functia',
+   post_de_munca.fir_angaj as 'firma angajatoare'
      from angajare
-       join persoana on
-         angajare.idnp = persoana.idnp
-         where persoana.idnp = '2008002000987';
+
+     join persoana on
+       angajare.idnp = persoana.idnp
+
+     join post_de_munca on
+       post_de_munca.id_post = angajare.id_post
+
+       where persoana.idnp = '2008002000987'
+
+       ;
